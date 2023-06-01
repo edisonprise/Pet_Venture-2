9a8b58f0c1964332f1aa91f5fa2ec50bbfecbe84
JesusEGomez <jesus.emanuel.gomez77@gmail.com>
2023-06-01 17:29:07 -0300

    inicializando develop

---
+++ src/app/home/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hola esto es el home</h1>
+    </div>
+  );
+}
---
+++ src/app/home/productos/page.js
@@ -0,0 +1,7 @@
+export default function Productos() {
+  return (
+    <div>
+      <h1>Hola esto es el Producto</h1>
+    </div>
+  );
+}
