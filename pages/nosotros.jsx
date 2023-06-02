3601e09023491863918ca15c3abdcf511b755060
JesusEGomez <jesus.emanuel.gomez77@gmail.com>
2023-06-02 20:08:06 -0300

    corrijo nosotros

--- pages/nosotros.jsx
+++ pages/nosotros.jsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 
 import Nosotros from "@/app/Components/Nosotros/Nosotros";
 
-=======
-import Nosotros from "@/app/Components/Nosotros/Nosotros";
->>>>>>> develop
 
 export default Nosotros; 
