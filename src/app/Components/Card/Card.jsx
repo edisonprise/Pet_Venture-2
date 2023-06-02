faeea1d7aada1607b3fe93da2327d868de43f3bb
ClaudioCabrera06 <claudiocabrera2001@gmail.com>
2023-06-02 19:34:44 -0300

    Confirmar cambios en archivos modificados

--- src/app/Components/Card/Card.jsx
+++ src/app/Components/Card/Card.jsx
@@ -1,31 +1,31 @@
-import React from "react";
+// import React from "react";
 
-//import { deleteDog, getDogs } from "../../redux/actions";
+// //import { deleteDog, getDogs } from "../../redux/actions";
 
-export default function CardComponent({ id, image, name, temperament, weight_min, weight_max, createdInDb, dispatch }) {
-//   function handleDelete(e, id) {
-//     e.preventDefault();
-//     dispatch(deleteDog(id));
-//     dispatch(getDogs());
-//   }
-  return (
-    <div className="card">
-      {createdInDb ? (
-        <div>
-          <img src={image} alt="img not found" />
-          <h3>{name}</h3>
-          <h5>Temperament: {temperament}</h5>
-          <h5>Weight: {weight_min} - {weight_max} lb</h5>
-          <button onClick={(e) => handleDelete(e, id)}>Delete</button>
-        </div>
-      ) : (
-        <div>
-          <img src={image} alt="img not found" />
-          <h3>{name}</h3>
-          <h5>Temperament: {temperament}</h5>
-          <h5>Weight: {weight_min} - {weight_max} lb</h5>
-        </div>
-      )}
-    </div>
-  );
-}; 
+// export default function CardComponent({ id, image, name, temperament, weight_min, weight_max, createdInDb, dispatch }) {
+// //   function handleDelete(e, id) {
+// //     e.preventDefault();
+// //     dispatch(deleteDog(id));
+// //     dispatch(getDogs());
+// //   }
+//   return (
+//     <div className="card">
+//       {createdInDb ? (
+//         <div>
+//           <img src={image} alt="img not found" />
+//           <h3>{name}</h3>
+//           <h5>Temperament: {temperament}</h5>
+//           <h5>Weight: {weight_min} - {weight_max} lb</h5>
+//           <button onClick={(e) => handleDelete(e, id)}>Delete</button>
+//         </div>
+//       ) : (
+//         <div>
+//           <img src={image} alt="img not found" />
+//           <h3>{name}</h3>
+//           <h5>Temperament: {temperament}</h5>
+//           <h5>Weight: {weight_min} - {weight_max} lb</h5>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }; 
--- src/app/Components/Paginate/Paginate.jsx
+++ src/app/Components/Paginate/Paginate.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-//import "./Paginated.css";
+// import React from "react";
+// //import "./Paginated.css";
 
-export default function Paginated({dogsPerPage, allDogs, paginated}){
-    const pageNumbers=[];
+// export default function Paginated({dogsPerPage, allDogs, paginated}){
+//     const pageNumbers=[];
 
-    for(let i=1; i<=Math.ceil(allDogs/dogsPerPage); i++){
-        pageNumbers.push(i);
-    }
+//     for(let i=1; i<=Math.ceil(allDogs/dogsPerPage); i++){
+//         pageNumbers.push(i);
+//     }
 
-    return(
-        <nav>
-            <ul className="pagination">
-                {pageNumbers?.map(number =>(
-                    <li className="number" key={number}>
-                        <button onClick={()=> paginated(number)}>{number}</button>
-                    </li>
-                ))}
+//     return(
+//         <nav>
+//             <ul className="pagination">
+//                 {pageNumbers?.map(number =>(
+//                     <li className="number" key={number}>
+//                         <button onClick={()=> paginated(number)}>{number}</button>
+//                     </li>
+//                 ))}
                 
-            </ul>
-        </nav>
-    )
-}; 
+//             </ul>
+//         </nav>
+//     )
+// }; 
