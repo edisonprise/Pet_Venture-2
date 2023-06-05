47705e151f634cf2011c8c986d8865d22ebf3983
Edinson <edisonprise@hotmail.com>
2023-06-05 11:49:36 -0500

    fixLinkCrearFormulario

--- src/app/components/NavBar/NavBar.jsx
+++ src/app/components/NavBar/NavBar.jsx
@@ -6,11 +6,11 @@ import SearchBar from "../SearchBar/SearchBar";
 
 const Navbar = () => {
   return (
-    <div className={styles.navbar}>
+    <div className={styles.navbar} >
       <div className={styles.logo}>
-      <img src={PetVenture.src} width="300px" height="100px" alt="Logo" />
+        <img src={PetVenture.src} width="300px" height="100px" alt="Logo" />
       </div>
-      <SearchBar/>
+      <SearchBar />
       <div className={styles.menu}>
         <ul className={styles.menuList}>
           <li>
@@ -26,7 +26,7 @@ const Navbar = () => {
             <Link href="/tienda">Tienda</Link>
           </li>
           <li>
-            <Link href="/formproducts">Producto</Link>
+            <Link href="/formulario">Crear Producto</Link>
           </li>
         </ul>
       </div>
