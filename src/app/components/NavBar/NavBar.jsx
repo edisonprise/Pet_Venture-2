a2a98886132899260bb7668f208721e955deb968
Eglemirvargas <eglemirvarmar@gmail.com>
2023-06-05 11:44:12 -0500

    nosotros listo, sin searchBar,contacto al inicio

--- src/app/components/NavBar/NavBar.jsx
+++ src/app/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 import PetVenture from "../../../../public/img/PetVenture.svg";
 import styles from "./Navbar.module.css";
-import SearchBar from "../SearchBar/SearchBar";
+
 
 const Navbar = () => {
   return (
@@ -10,18 +10,14 @@ const Navbar = () => {
       <div className={styles.logo}>
       <img src={PetVenture.src} width="300px" height="100px" alt="Logo" />
       </div>
-      <SearchBar/>
+      
       <div className={styles.menu}>
         <ul className={styles.menuList}>
           <li>
             <Link href="/nosotros">Nosotros</Link>
           </li>
-          <li>
-            <Link href="/contacto">Contacto</Link>
-          </li>
-          <li>
-            <Link href="/adopta">Adopta</Link>
-          </li>
+         
+
           <li>
             <Link href="/tienda">Tienda</Link>
           </li>
