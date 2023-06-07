5ca4521bad2b8974e0a3a56727b1ca28f993c47f
TomasHas <thastrup@gmail.com>
2023-06-07 15:29:21 -0300

    details, up poco mas ordenado

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -11,46 +11,46 @@ function productDetail({ product }) {
     <div>
       <div className={styles.container}>
         <img className={styles.img} src={product[0].image} alt="Not found" />
-      </div>
-      <div className={styles.infoContainer}>
-        <div>
-          <h2>Name</h2> {product[0].name}
-        </div>
+        <div className={styles.infoContainer}>
+          <div className={styles.propertyContainer}>
+            <h4>Name</h4> <div>{product[0].name} </div>
+          </div>
 
-        <div>
-          <h2>Brand</h2>
-          <span> {product[0].brand}</span>
-        </div>
+          <div className={styles.propertyContainer}>
+            <h4>Brand</h4>
+            <span> {product[0].brand}</span>
+          </div>
 
-        <div>
-          <h2>Category</h2>
-          {product[0].category}
-        </div>
+          <div className={styles.propertyContainer}>
+            <h4>Category</h4>
+            <span> {product[0].category}</span>
+          </div>
 
-        <div>
-          <h2>Descripcion</h2>
-          <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-            sed quia non numquam eius modi tempora incidunt ut labore et dolore
-            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-            aliquid ex ea commodi consequatur? Quis autem vel eum iure
-            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
-            pariatur?
-          </p>
-        </div>
+          <div className={styles.propertyContainer}>
+            <h4>Descripcion</h4>
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+              velit, sed quia non numquam eius modi tempora incidunt ut labore
+              et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
+              veniam, quis nostrum exercitationem ullam corporis suscipit
+              laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
+              vel eum iure reprehenderit qui in ea voluptate velit esse quam
+              nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+              voluptas nulla pariatur?
+            </p>
+          </div>
 
-        <div>
-          <h2>Description</h2> <h2>Price</h2>
-          {product[0].price}
-        </div>
+          <div className={styles.propertyContainer}>
+            <h4>Price</h4>
+            <span> {product[0].price} </span>
+          </div>
+        </div>{" "}
       </div>
     </div>
   );
