3303ec2eaa5d4056b4cc8184e22bc89fa5a116ac
JesusEGomez <111249837+JesusEGomez@users.noreply.github.com>
2023-06-12 19:17:35 -0300

    Update Card.jsx

--- src/app/Components/Card/Card.jsx
+++ src/app/Components/Card/Card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./card.module.css";
+import styles from "./Card.module.css";
 import Link from "next/link";
 import { addCarrito, deleteCarrito } from "../../../../redux/actions"
 import { useDispatch } from "react-redux";
