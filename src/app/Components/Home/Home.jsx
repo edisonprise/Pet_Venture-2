ec84f1630b9757594d14058e0674dc10ef92ee37
JesusEGomez <jesus.emanuel.gomez77@gmail.com>
2023-06-16 19:32:26 -0300

    agregando compras a la base de datos

--- src/app/Components/Home/Home.jsx
+++ src/app/Components/Home/Home.jsx
@@ -1,23 +1,6 @@
 import React, { useEffect } from "react";
 import Navbar from "../NavBar/NavBar";
 import Slider from "../Slider/Slider.jsx";
-<<<<<<< HEAD
-import { onAuthStateChanged } from "firebase/auth";
-import {
-  auth,
-  registerNewUser,
-  userExist,
-  getUserInfo,
-} from "@/app/firebase/firebaseConfig";
-import Ofertas from "../Ofertas/Ofertas";
-import Ofertas2 from "../Ofertas2/Ofertas2";
-import Footer from "../Footer/Footer";
-import { setUserInfo, setUserState } from "../../../../redux/actions";
-import { useDispatch, useSelector } from "react-redux";
-import { getProducts } from "../../../../redux/actions";
-
-import addDocuments from "@/app/firebase/firebaseConfig";
-=======
 import Ofertas from "../Ofertas/Ofertas";
 import Ofertas2 from "../Ofertas2/Ofertas2";
 import Footer from "../Footer/Footer";
@@ -26,7 +9,6 @@ import addDocuments from "@/app/Firebase/firebaseConfig";
 import { useDispatch } from "react-redux";
 import { handleAuthStateChanged } from "@/app/utils/handleAuthStateChanged";
 import { registerNewPurchase } from "@/app/Firebase/firebaseConfig";
->>>>>>> cec57955ce202e8c811fa9b3c57f0533bd251802
 
 import styles from "./Home.module.css";
 import Swal from "sweetalert2";
@@ -35,36 +17,6 @@ export default function Home() {
   const products = useSelector((state) => state.products);
 
   useEffect(() => {
-<<<<<<< HEAD
-    onAuthStateChanged(auth, async (user) => {
-      console.log(user);
-      if (user) {
-        const isRegistered = userExist(user.uid);
-        if (isRegistered) {
-          const userInfo = await getUserInfo(user.uid);
-          if (userInfo?.processCompleted) {
-            dispatch(setUserState(3));
-            dispatch(setUserInfo(userInfo));
-          } else {
-            await registerNewUser({
-              uid: user.uid,
-              displayName: user.displayName,
-              profilePicture: user.photoURL,
-              username: "",
-              processCompleted: false,
-              carrito: [],
-              compras: [],
-            });
-            dispatch(setUserState(2));
-            dispatch(setUserInfo(userInfo));
-          }
-        }
-      } else {
-        dispatch(setUserState(1));
-      }
-    });
-  }, []);
-=======
     handleAuthStateChanged(dispatch)
     const urlParams = new URLSearchParams(window.location.search);
     const status = urlParams.get("status")
@@ -91,7 +43,6 @@ export default function Home() {
     cleanUrl()
   }, [])
 
->>>>>>> cec57955ce202e8c811fa9b3c57f0533bd251802
   // ! Esta funcion esta comenentada para después poder cargar productos
   const handlerClick = () => {
     addDocuments();
